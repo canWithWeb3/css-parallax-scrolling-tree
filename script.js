0cfb08e397f20f50f0417b25963fbd53001ccc35
newSoft5 <92645540+newsoft5@users.noreply.github.com>
2023-07-04 22:15:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+( () => {
+    for(let x=0; x<200; x++){
+        console.log(x)
+    }
+})() 
+
+( () => {
+    console.log("no")
+})()
+
+
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+window.addEventListener("scroll", () => {
+    let value = window.scrollY
+
+    text.style.marginTop = value * 2.5 + "px"
+    leaf.style.top = value * -1.5 + "px"
+    leaf.style.left = value * 1.5 + "px"
+    hill5.style.left = value * 1.5 + "px"
+    hill4.style.left = value * -1.5 + "px"
+    hill1.style.top = value * 1 + "px"
+})
+
+
+
+
